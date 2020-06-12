e81d3527f1e1e3e6b2593497343c78977a8534b1
Anik Ghosh <tech.anikghosh@gmail.com>
2020-06-12 21:11:28 +0530

    third commit

--- validator/user.validator.js
+++ validator/user.validator.js
@@ -1,6 +1,5 @@
-const { check } = require ('express-validator');
+const {check} = require('express-validator');
 const User = require('../model/user.model');
-
 // const unique_email = (email) => {
 //     User.findOne({email}, (err, data) => {
 //         if (err) {
@@ -16,56 +15,60 @@ const User = require('../model/user.model');
 //         };
 //     });
 // }
-const is_unique = (data, type) => {
+const is_unique = async (data, type) => {
     if (type === 'email') {
-        obj = { email:data };
+        obj = {email: data};
     } else if (type === 'username') {
-        obj = { username:data };
+        obj = {username: data};
     }
-    User.findOne(obj, (err, data) => {
+    result=null
+     await User.findOne(obj, (err, data) => {
         if (err) {
             // Internal server error
             res.status(500).send({msg: "Internal Server Error"});
         } else {
-            if (!data) {
-                return true;
+            if (data === null) {
+                console.log('returning unique');
+                result=true;
+            } else {
+                console.log('returning not unique');
+                result=false;
             }
-            else {
-                console.log('data--> ', data);
-                return false;
-            }
-        };
+        }
+        ;
     });
-
+    return result
 }
 const user_validator = {};
-
 user_validator.check_username = () => {
-    return check('username').not().isEmpty().custom(username => {
-        let result = is_unique(username,'username');
-        console.log(result);
+    return check('username').not().isEmpty().custom(async username => {
+        result = await is_unique(username, 'username')
+        console.log(result)
         if (!result) {
-            throw new Error('Username already taken')
-          }
-          return username;
+            console.log('User name taken')
+            throw new Error('User name taken')
+        }
+        console.log('unique username')
+        return username;
     })
 }
-
 user_validator.check_email = () => {
-    return check('email').not().isEmpty().isEmail().custom(email => {
-        if (!is_unique(email,'email')) {
-        throw new Error('Email already registered')
-      }
-      return email;
+    return check('email').not().isEmpty().isEmail().custom(async email => {
+        // console.log('email --> ', is_unique(email,'email'));
+        result = await is_unique(email, 'email')
+        console.log(result)
+        if (!result) {
+            console.log('Email taken')
+            throw new Error('Email taken')
+        }
+        console.log('unique email')
+        return email;
     })
 }
-
 user_validator.check_password = () => {
     return check('password').not().isEmpty();
 }
-
 user_validator.check_phone = () => {
     return check('phone').not().isEmpty().isMobilePhone();
 }
-
 module.exports = user_validator;
