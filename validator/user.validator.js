b976240b5d67ca2b5cd6b27f1930c69cd9e58ff3
Anik Ghosh <tech.anikghosh@gmail.com>
2020-06-12 18:15:26 +0530

    third commit

--- validator/user.validator.js
+++ validator/user.validator.js
@@ -16,32 +16,35 @@ const User = require('../model/user.model');
 //         };
 //     });
 // }
-const is_unique = async (data, type) => {
+const is_unique = (data, type) => {
     if (type === 'email') {
         obj = { email:data };
     } else if (type === 'username') {
         obj = { username:data };
     }
-    await User.findOne(obj, (err, data) => {
+    User.findOne(obj, (err, data) => {
         if (err) {
             // Internal server error
             res.status(500).send({msg: "Internal Server Error"});
         } else {
-            if (data === null) {
+            if (!data) {
                 return true;
             }
             else {
+                console.log('data--> ', data);
                 return false;
             }
         };
     });
+
 }
 const user_validator = {};
 
 user_validator.check_username = () => {
     return check('username').not().isEmpty().custom(username => {
-        console.log('username --> ', is_unique(username,'username'));
-        if (!is_unique(username,'username')) {
+        let result = is_unique(username,'username');
+        console.log(result);
+        if (!result) {
             throw new Error('Username already taken')
           }
           return username;
@@ -50,7 +53,6 @@ user_validator.check_username = () => {
 
 user_validator.check_email = () => {
     return check('email').not().isEmpty().isEmail().custom(email => {
-        console.log('email --> ', is_unique(email,'email'));
         if (!is_unique(email,'email')) {
         throw new Error('Email already registered')
       }
