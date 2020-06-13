a172265f4b439bb8d81a867e47aa6b6b7c9f53b8
Anik Ghosh <tech.anikghosh@gmail.com>
2020-06-13 19:36:18 +0530

    commit 4

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -55,7 +55,7 @@ const user_controller = {
                         if (await bcrypt.compare(password, data.password)) {
                             const accessToken = jwt.sign({name: username}, 'verysecretkey')
                             // res.status(200).json({accessToken});
-                            res.cookie('awtToken',accessToken, { maxAge: 900000, httpOnly: true})
+                            res.cookie('awtToken',accessToken, { maxAge: 900000, httpOnly: true, sameSite: 'strict', secure: true});
                             return res.redirect('/');
                         } else {
                             res.status(401).send('Unauthorized access');
