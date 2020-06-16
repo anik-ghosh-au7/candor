a298d86ee6c24246ed8b17e859534803a6c79888
Kumar Atul <atultunnu2@gmail.com>
2020-06-16 13:49:37 +0530

    extension routes are consoled. Add form to send to server.

--- candor_extension_dev/main_popup.js
+++ candor_extension_dev/main_popup.js
@@ -2,17 +2,26 @@ chrome.storage.local.get('username', (result) => {
     document.getElementById("username").innerHTML = "Hello " + result.username;
 });
 var curr_url;
-var server_url="http://localhost:3000/post/";
+var server_url = "http://localhost:3000/post/";
 chrome.runtime.sendMessage(
     {payload: 'Give active tab'}, (data) => {
         curr_url = data;
         // console.log("Mission completee", curr_url);
-        update_route()
-    });
-function update_route(){
-    document.getElementById("question").setAttribute('href','');
-    document.getElementById("related").setAttribute('href','');
-    document.getElementById("admin").setAttribute('href','');
-    document.getElementById("others").setAttribute('href','');
+        update_links();
+    }
+);
+
+
+function update_links() {
+    document.getElementById("question").addEventListener("click", myFunction('question'));
+    document.getElementById("related").addEventListener("click", myFunction('related'));
+    document.getElementById("admin").addEventListener("click", myFunction('admin'));
+    document.getElementById("others").addEventListener("click", myFunction('others'));
 }
 
+
+function myFunction(type) {
+    return () => {
+        console.log(type, curr_url)
+    }
+}
