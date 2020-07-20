b2fa20829428dd69d0d41b69851721c2d3c42c6b
kumar-atul-au7 <atultunnu2@gmail.com>
2020-07-20 20:53:14 +0530

    Message for non-friend implementing

--- src/controller/message.controller.js
+++ src/controller/message.controller.js
@@ -1,77 +1,95 @@
 import User from '../model/user.model';
 import webpush from 'web-push';
 
-const message_controller={
-    handle_messages: async function (req,res) {
+const message_controller = {
+    handle_messages: async function (req, res) {
         let flag = false;
+        let isFriend = false;
+        await User.findOne({username: req.body.user}).then(result => {
+            result.friend_list.forEach((friend) => {
+                if (friend === req.body.share_username) isFriend = true;
+            })
+        });
         // console.log('body',req.body);
-        await User.findOneAndUpdate({username:req.body.share_username},{
-                    "$push": {
-                        "received_messages": {
-                            sender: req.body.user,
-                            shared_url: req.body.context,
-                            msg_body: req.body.comments
-                        }
+        if (isFriend) {
+            await User.findOneAndUpdate({username: req.body.share_username}, {
+                "$push": {
+                    "received_messages": {
+                        sender: req.body.user,
+                        shared_url: req.body.context,
+                        msg_body: req.body.comments
                     }
-                })
+                }
+            })
                 .then((result) => {
-                    if(result){
+                    if (result) {
                         console.log("receiver message saved");
                         // res.status(200).send('Message sent');
                         // let message = req.body.comments.length < 30 ? req.body.comments : req.body.comments.slice(0, 29) + '...';
-                        let payload = JSON.stringify({ 
-                        title: `New message from ${req.body.user}`,
-                        msg_body: req.body.comments
-                    });
-                    User.findOne({username: req.body.share_username}).then(result => {
-                        // console.log('sub_obj : ', JSON.parse(result.subscription));
-                        result.subscription.forEach(element => {
-                            webpush.sendNotification(JSON.parse(element), payload)
+                        let payload = JSON.stringify({
+                            title: `New message from ${req.body.user}`,
+                            msg_body: req.body.comments
                         });
-                    }).catch(err => console.error(err));
+                        User.findOne({username: req.body.share_username}).then(result => {
+                            // console.log('sub_obj : ', JSON.parse(result.subscription));
+                            result.subscription.forEach(element => {
+                                webpush.sendNotification(JSON.parse(element), payload)
+                            });
+                        }).catch(err => console.error(err));
                         flag = true;
-                    }else{
+                    } else {
                         // res.status(400).send("Username doesn't exist");
                         flag = false;
                     }
                 })
                 .catch(err => console.log(err));
-
-            if (flag) {
-                User.findOneAndUpdate({username:req.body.user},{
-                    "$push": {
-                        "sent_messages": {
-                            reciever: req.body.share_username,
-                            shared_url: req.body.context,
-                            msg_body: req.body.comments
-                        }
+        } else {
+            await User.findOneAndUpdate({username: req.body.share_username}, {
+                "$push": {
+                    "other_messages": {
+                        sender: req.body.user,
+                        shared_url: req.body.context,
+                        msg_body: req.body.comments
                     }
-                })
+                }
+            }).then(() => console.log('msg saved in others')).catch(err => console.log(err));
+        };
+
+        if (flag) {
+            User.findOneAndUpdate({username: req.body.user}, {
+                "$push": {
+                    "sent_messages": {
+                        reciever: req.body.share_username,
+                        shared_url: req.body.context,
+                        msg_body: req.body.comments
+                    }
+                }
+            })
                 .then(() => {
                     console.log('sender message saved');
                     res.status(200).send('Message sent');
                 })
                 .catch(err => console.log(err));
-            } else {
-                res.status(400).send("Username doesn't exist");
-            }
+        } else {
+            res.status(400).send("Username doesn't exist");
+        }
     },
 
-    handle_post_comment_messages : function (receiver, sender, url, body, title) {
-        User.findOneAndUpdate({username:receiver},{
-                    "$push": {
-                        "received_messages": {
-                            sender: sender,
-                            shared_url: url,
-                            msg_body: body
-                        }
-                    }
-                })
-                .then((result) => {
-                    if(result){
-                        console.log("receiver message saved");
-                        let message = body.length < 30 ? body : body.slice(0, 29) + '...';
-                        let payload = JSON.stringify({ 
+    handle_post_comment_messages: function (receiver, sender, url, body, title) {
+        User.findOneAndUpdate({username: receiver}, {
+            "$push": {
+                "received_messages": {
+                    sender: sender,
+                    shared_url: url,
+                    msg_body: body
+                }
+            }
+        })
+            .then((result) => {
+                if (result) {
+                    console.log("receiver message saved");
+                    let message = body.length < 30 ? body : body.slice(0, 29) + '...';
+                    let payload = JSON.stringify({
                         title: title,
                         msg_body: message
                     });
@@ -81,40 +99,40 @@ const message_controller={
                         });
                         return;
                     }).catch(err => console.error(err));
-                    }
-                })
-                .catch(err => console.log(err));
-            },
+                }
+            })
+            .catch(err => console.log(err));
+    },
 
     handle_requests: function (receiver, body, title) {
-            User.findOne({username:receiver})
-                    .then((result) => {
-                        if(result.subscription){
-                            let message = body;
-                            let payload = JSON.stringify({ 
-                            title: title,
-                            msg_body: message
-                                 });
-                            result.subscription.forEach(elem => {
-                                webpush.sendNotification(JSON.parse(elem), payload)
-                            })
-                        }
+        User.findOne({username: receiver})
+            .then((result) => {
+                if (result.subscription) {
+                    let message = body;
+                    let payload = JSON.stringify({
+                        title: title,
+                        msg_body: message
+                    });
+                    result.subscription.forEach(elem => {
+                        webpush.sendNotification(JSON.parse(elem), payload)
                     })
-                    .catch(err => console.log(err));
+                }
+            })
+            .catch(err => console.log(err));
 
     },
 
-    getmsg: function (req,res) {
-        let messages={};
-        User.findOne({username:req.user.name}).then((result)=>{
+    getmsg: function (req, res) {
+        let messages = {};
+        User.findOne({username: req.user.name}).then((result) => {
             // console.log(result);
-            messages.received=result.received_messages;
-            messages.sent=result.sent_messages;
-            messages.username=req.user.name;
-            res.render('msg_inbox_outbox',{messages});
+            messages.received = result.received_messages;
+            messages.sent = result.sent_messages;
+            messages.username = req.user.name;
+            res.render('msg_inbox_outbox', {messages});
         });
-            // res.render('msg_inbox_outbox',[{'sent':messages.sent},{'received':messages.received}]);
+        // res.render('msg_inbox_outbox',[{'sent':messages.sent},{'received':messages.received}]);
 
     }
 };
-module.exports=message_controller;
+module.exports = message_controller;
