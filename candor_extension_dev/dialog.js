45e5d056b89b014d565cb4fd02e9ea25dc9b8f35
Anik Ghosh <tech.anikghosh@gmail.com>
2020-08-09 14:01:59 +0530

    speech Synthesis and ringtone added for calls

--- candor_extension_dev/dialog.js
+++ candor_extension_dev/dialog.js
@@ -9,7 +9,31 @@ window.onload=()=>{
     document.getElementById('profile_img').src=caller_img;
     document.getElementById('caller_name').innerText = 'You are receiving a call from ' + caller;
     document.getElementById('accept').onclick="window.open('http://localhost:3000/communication/video?friend_username=king&caller=true')"
-    document.getElementById('reject').onclick=()=>{
-    }
-    chrome.send
+    document.getElementById('reject').onclick=()=>{}
+    function Ringtone(n) {
+        if (n >= 10) return
+        if ('speechSynthesis' in window) {
+            var msg = new SpeechSynthesisUtterance();
+            msg.voiceURI = 'native';
+            // var caller = 'Anik';
+            msg.volume = 2;
+            msg.rate = 0.7;
+            msg.pitch = 2;
+            msg.text = `You are getting a call from ${caller}`;
+            msg.lang = 'en';
+            window.speechSynthesis.speak(msg);
+            msg.onend = setTimeout(() => {
+                Ringtone(++n);
+                }, 5000);
+        }else{
+            var sound      = document.createElement('audio');
+            sound.id       = 'audio-player';
+            sound.controls = 'controls';
+            sound.src      = 'ringtone.mp3';
+            sound.type     = 'audio/mpeg';
+            document.getElementById('ringtone').appendChild(sound);
+            sound.play();
+        }
+    };
+    Ringtone(0);
 };
